f3dac6f79d9b4314dc3a4de094c7a7dac973312a
KingCobra <hd601256@gmail.com>
2017-06-27 23:59:11 -0600

    Create filerecursion.h

---
+++ Hell/filerecursion.h
@@ -0,0 +1,12 @@
+#ifndef filethisshit
+#define filethisshit
+#include <errno.h>
+#include "dir.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+void start(const char *infile, int enc);
+//note was here :)
+
+#endif
+
